dd512f45cadd1dacbcddb86c614c7388252f4e22
Jorge <jorgediazok@gmail.com>
2020-07-05 20:07:41 -0300

    building form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const engine = require('ejs-mate');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
 //Settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+
+//Routes
+app.use('/', require('./routes/index'));
 
 //Starting server
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {});
+
+router.get('/signin', (req, res, next) => {});
+
+router.post('/signin', (req, res, next) => {});
 
 module.exports = router;
